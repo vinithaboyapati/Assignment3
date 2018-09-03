0b6bab4be0509266994d70a1934c574934af8f0a
edureka <edureka@master.yourdomain.com>
2018-09-03 11:45:01 +0530

    first commit

---
+++ src/main/java/co/aricent/com/addressbook/App.java
@@ -0,0 +1,13 @@
+package co.aricent.com.addressbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
